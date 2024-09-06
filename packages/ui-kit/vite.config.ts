bd61c310a28e5dd5f373c23572c7667bde8a4f9a
cqh963852 <cqh963852@hotmail.com>
2024-09-06 14:55:35 +0800

    chore: update build config

--- packages/ui-kit/vite.config.ts
+++ packages/ui-kit/vite.config.ts
@@ -1,52 +1,26 @@
-import type { UserConfig } from 'vite';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
-import vueSetupExtend from 'unplugin-vue-setup-extend-plus/vite';
-import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import dts from 'vite-plugin-dts';
 
-export default defineConfig((): UserConfig => {
-  return {
-    base: './',
-    server: {
-      port: 3456,
-      host: true,
+export default defineConfig({
+  plugins: [tsconfigPaths(), vue(), vueJsx(), dts()],
+  build: {
+    outDir: 'dist',
+    lib: {
+      entry: ['src/index.ts', 'src/packages/styles/index.scss'], // 组件库的入口文件
+      fileName: (format) => `${format}/index.js`, // 构建输出的文件名
+      formats: ['es', 'cjs'],
     },
-    esbuild: {
-      pure: ['console.log', 'debugger'],
-    },
-    plugins: [
-      tsconfigPaths(),
-      vue(),
-      // have to
-      vueJsx({
-        /*include: [/\.vue$/, /\.tsx?$/, /\.jsx?$/]*/
-      }),
-      vueSetupExtend({
-        /* enableAutoExpose 允许自动抛出 */
-      }),
-      cssInjectedByJsPlugin(),
-      dts(),
-    ],
-    build: {
-      outDir: 'dist',
-      lib: {
-        entry: 'src/index.ts',
-        name: 'vue-ui-kit_ant',
-        fileName: (format) => `index.${format}.js`,
-      },
-      rollupOptions: {
-        // 确保外部化处理那些你不想打包进库的依赖
-        external: ['vue', 'ant-design-vue', '@ant-design/icons-vue'],
-        output: {
-          // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
-          globals: {
-            vue: 'Vue',
-          },
+    rollupOptions: {
+      input: ['src/index.ts', 'src/packages/styles/index.scss'],
+      external: ['vue', 'ant-design-vue', '@ant-design/icons-vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
         },
       },
-    }
-  };
+    },
+  },
 });
